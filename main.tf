df1ae7291168f0145b7a493fc4403d3789dee0b6
Mamta <mamta.bourai2107@gmail.com>
2026-04-21 13:11:33 +0530

    added storage account

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "mannat" {
   name = "rg-mannat"
   location = "north india"
 }
+
+resource "azurerm_storage_account" "mannats" {
+    name = "storage-mannat"
+    resource_group_name = azurerm_resource_group.mannat.name
+    location = azurerm_resource_group.mannat.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+}
