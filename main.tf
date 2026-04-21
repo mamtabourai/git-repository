1d3a137aea4e0d4ecafcabfde8118b6515a91472
Mamta <mamta.bourai2107@gmail.com>
2026-04-21 13:53:53 +0530

    backend added

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "mannat" {
-  name = "rg-mannat"
-  location = "north india"
+  name     = "rg-mannat"
+  location = "south india"
 }
 
 resource "azurerm_storage_account" "mannats" {
-    name = "storage-mannat"
-    resource_group_name = azurerm_resource_group.mannat.name
-    location = azurerm_resource_group.mannat.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
+  name                     = "storagemannat"
+  resource_group_name      = azurerm_resource_group.mannat.name
+  location                 = azurerm_resource_group.mannat.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "mannatc" {
-    name = "container-mannat"
-    storage_account_name = azurerm_storage_account.mannats.name
-    container_access_type = "private"
+  name                  = "container-mannat"
+  storage_account_name  = azurerm_storage_account.mannats.name
+  container_access_type = "private"
 }
--- provider.tf
+++ provider.tf
@@ -1,15 +1,22 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-mannat"
+    storage_account_name = "storagemannat"
+    container_name       = "container-mannat"
+    key                  = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "4.69.0"
+      source  = "hashicorp/azurerm"
+      version = "4.69.0"
     }
   }
 }
 
 provider "azurerm" {
   features {
-    
+
   }
   subscription_id = "69aaf1e8-4cfa-4d3f-aa59-f2a896045552"
 }
