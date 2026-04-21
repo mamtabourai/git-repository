d18808c43c40c1ee60f12c7c6e9ceda421f1e081
Mamta <mamta.bourai2107@gmail.com>
2026-04-21 13:19:25 +0530

    storage container added

--- main.tf
+++ main.tf
@@ -10,3 +10,9 @@ resource "azurerm_storage_account" "mannats" {
     account_tier = "Standard"
     account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_container" "mannatc" {
+    name = "container-mannat"
+    storage_account_name = azurerm_storage_account.mannats.name
+    container_access_type = "private"
+}
